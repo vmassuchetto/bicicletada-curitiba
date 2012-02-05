5ceca7dc3f1fa2d4fa9682daa851d89c0c0d1465
Vinicius Massuchetto <viniciusmassuchetto@gmail.com>
2012-02-05 17:07:14 -0200

    Correção do link para o Identi.ca.

--- themes/bicicletada/functions.php
+++ themes/bicicletada/functions.php
@@ -97,7 +97,7 @@ function share_box() {
     <div class="share-box">
 
         <div class="box identica">
-            <iframe height="61" width="61" scrolling="no" frameborder="0" src="http://www.tildehash.com/identishare.php?noscript&title=<?php echo $p->post_title . ' ' . get_permalink($post->ID); ?>&style2" border="0" marginheight="0" marginwidth="0" allowtransparency="true" style="width:55px; height:70px;"></iframe>
+            <iframe height="61" width="61" scrolling="no" frameborder="0" src="http://www.tildehash.com/identishare.php?noscript&title=<?php echo $post->post_title . ' ' . get_permalink($post->ID); ?>" border="0" marginheight="0" marginwidth="0" allowtransparency="true" style="width:55px; height:70px;"></iframe>
         </div>
 
         <div class="box facebook">
